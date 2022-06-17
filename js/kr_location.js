cf21babac318f926b4563465bc4e1416ce39d860
Montegrand <hpt0311@naver.com>
2022-06-17 17:11:51 +0900

    220617

--- js/kr_location.js
+++ js/kr_location.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     let wid = window.innerWidth;
     let hei = window.innerHeight;
 
@@ -6,8 +6,8 @@ window.onload = function() {
 
     navLi.forEach(function (v, n) {
         if (wid <= 1240) {
-            for ( i = 0; i < navLi.length; i++ ) {
-                if ( i !== 7 ) {
+            for (i = 0; i < navLi.length; i++) {
+                if (i !== 7) {
                     navLi[i].href = `#`;
                 }
             }
@@ -16,9 +16,11 @@ window.onload = function() {
 
     var allA = document.querySelectorAll(`a`);
 
-    allA.forEach(function(v,n){
+    allA.forEach(function (v, n) {
         if (v.href.search(`#`) > 0) {
-            v.onclick = function() {return false;}
+            v.onclick = function () {
+                return false;
+            }
         }
     })
 
@@ -49,5 +51,28 @@ window.onload = function() {
             }
         }
     })
+
+    // const mapContainer = document.querySelector('section .map'),
+    //     mapOption = {
+    //         center: new kakao.maps.LatLng(36.5, 127.5), // 지도의 중심 좌표(임의 설정)
+    //         level: 13 // 지도의 확대 레벨(임의 설정)
+    //     };
+
+    // //설정한 지도 생성
+    // const map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // //마커 초기화(초기화 시 지도에 미리 지정 가능 : 카카오맵 API 문서 참조)
+    // const marker = new kakao.maps.Marker();
+
+    // //카카오맵 클릭 이벤트 추가
+    // kakao.maps.event.addListener(map, 'click', (mouseEvent) => {
+    //     //클릭한 위도, 경도 정보 불러오기
+    //     const latlng = mouseEvent.latLng;
+    //     //마커 위치를 클릭한 위치로 이동
+    //     marker.setPosition(latlng);
+    //     marker.setMap(map);
+
+    //     alert(`위도 : ${latlng.getLat()}, 경도 : ${latlng.getLng()}`);
+    // });
 
 }
