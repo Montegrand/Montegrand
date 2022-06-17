c7d66e4a76e348b9e0bf17457db330fc87391406
Montegrand <hpt0311@naver.com>
2022-06-17 10:47:44 +0900

    220617

--- js/kr_pr_center.js
+++ js/kr_pr_center.js
@@ -1,13 +1,27 @@
-window.onload = function() {
+window.onload = function () {
+
+    let wid = window.innerWidth;
+    let hei = window.innerHeight;
+
+    let navLi = document.querySelectorAll(`header .top_nav.wrap > li > a, header .top_nav.wrap .depth.snd > li > a`);
+    console.log(navLi);
+    navLi.forEach(function (v, n) {
+        if (wid <= 1240) {
+            v.href = `#`;
+        }
+    })
 
     var allA = document.querySelectorAll(`a`);
 
-    allA.forEach(function(v,n){
+
+    allA.forEach(function (v, n) {
         if (v.href.search(`#`) > 0) {
-            v.onclick = function() {return false;}
+            v.onclick = function () {
+                return false;
+            }
         }
     })
-    
+
     let menu = document.querySelector("header .bt");
     let nav = document.querySelector("header .top_nav.wrap");
     let overay = document.querySelector("header .overay")
@@ -35,16 +49,14 @@ window.onload = function() {
             }
         }
     })
-    
-    let wid = screen.width;
-    let hei = screen.height;
+
     let tap = document.querySelectorAll(`section .tap a`);
     let contents = document.querySelectorAll(`section .contents > div`);
     let brochure = document.querySelectorAll(`section .brochure > a`)
-    tap.forEach(function(v, n) {
-        v.addEventListener('click', function(e) {
-            for ( i = 0; i <= tap.length-1; i++ ) {
-                if ( tap[i] === e.target ) {
+    tap.forEach(function (v, n) {
+        v.addEventListener('click', function (e) {
+            for (i = 0; i <= tap.length - 1; i++) {
+                if (tap[i] === e.target) {
                     tap[i].classList.add(`on`);
                     contents[i].classList.add(`on`);
                 } else {
@@ -53,33 +65,38 @@ window.onload = function() {
                 }
             };
         })
-        // console.log(v.style)
     })
-    brochure.forEach(function(v,n) {
-        if ( wid > hei ) {
-            v.style.width = `1500`;
+
+    window.addEventListener('resize', function () {
+        if ( window.innerWidth > window.innerHeight ) {
+            return document.querySelector(`section .brochure`).classList.add(`width`);
+        } else {
+            return document.querySelector(`section .brochure`).classList.remove(`width`);
         }
     })
+
+    // window.addEventListener('resize', function () {
+    //     if (window.matchMedia('(orientation: portrait)').matches) {
+    //         return document.querySelector(`section .brochure`).classList.remove(`width`);
+    //     } else {
+    //         return document.querySelector(`section .brochure`).classList.add(`width`);
+    //     }
+    // });
+
+
+
 
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
 
-    console.log(wid);
 
-    let navLi = document.querySelectorAll(`header .top_nav.wrap > li > a, header .top_nav.wrap .depth.snd > li > a`);
-    console.log(navLi);
-    navLi.forEach(function(v,n) {
-        if ( wid <= 1240 ) {
-            v.href = `#`;
-        }
-    })
     // let money = document.querySelectorAll(`.money`)
 
     // money.forEach(function(v,n) {
     //     // console.log(v.innerHTML);
     //     v.innerHTML = numberWithCommas(v.innerHTML);
     // })
-    
+
 }
