0ab38a5d7fffb493429e1f61170fd4283c280150
Montegrand <hpt0311@naver.com>
2022-06-21 18:29:44 +0900

    220621

--- js/kr_location.js
+++ js/kr_location.js
@@ -76,8 +76,48 @@ window.onload = function () {
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
 
-    var iwContent = '<div>계룡건설산업 본사<br><a href="https://map.kakao.com/link/map/Hello World!,36.3446724536476, 127.38407076936737" style="color:blue;  font-size: 1rem;" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue;  font-size: 1rem;" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+    var iwContent = '<div>계룡건설산업 본사<br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue;  font-size: 1rem;" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue;  font-size: 1rem;" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+    function getInfo() {
+        // 지도의 현재 중심좌표를 얻어옵니다 
+        var center = map.getCenter(); 
+        
+        // 지도의 현재 레벨을 얻어옵니다
+        var level = map.getLevel();
+        
+        // 지도타입을 얻어옵니다
+        var mapTypeId = map.getMapTypeId(); 
+        
+        // 지도의 현재 영역을 얻어옵니다 
+        var bounds = map.getBounds();
+        
+        // 영역의 남서쪽 좌표를 얻어옵니다 
+        var swLatLng = bounds.getSouthWest(); 
+        
+        // 영역의 북동쪽 좌표를 얻어옵니다 
+        var neLatLng = bounds.getNorthEast(); 
+        
+        // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
+        var boundsStr = bounds.toString();
+        
+        
+        var message = '지도 중심좌표는 위도 ' + center.getLat() + ', <br>';
+        message += '경도 ' + center.getLng() + ' 이고 <br>';
+        message += '지도 레벨은 ' + level + ' 입니다 <br> <br>';
+        message += '지도 타입은 ' + mapTypeId + ' 이고 <br> ';
+        message += '지도의 남서쪽 좌표는 ' + swLatLng.getLat() + ', ' + swLatLng.getLng() + ' 이고 <br>';
+        message += '북동쪽 좌표는 ' + neLatLng.getLat() + ', ' + neLatLng.getLng() + ' 입니다';
+        
+        // 개발자도구를 통해 직접 message 내용을 확인해 보세요.
+        console.log(message);
+    }
+    getInfo();
+
+    
+
+    // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+    // searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
     // 인포윈도우를 생성합니다
     var infowindow = new kakao.maps.InfoWindow({
@@ -93,6 +133,4 @@ window.onload = function () {
         // 마우스 휠로 지도 확대,축소 가능여부를 설정합니다
         map.setZoomable(zoomable);
     }
-
-
 }
