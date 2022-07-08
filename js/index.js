91b91a2be1a922fb49f4c97ea7c3787f68ca0712
Montegrand <hpt0311@naver.com>
2022-07-08 12:56:07 +0900

    220708

--- js/index.js
+++ js/index.js
@@ -2,103 +2,160 @@ window.onload = function () {
 
     let textC = ''
 
-    textC += 'Welcome my PORTFOLIO<br><br>Thank you sincerely for your kind visit.<br>It meant a lot to me that you took the time to come by.<br>I know how busy you are, and i truly value the time we spent together<br><br>Please konw that I am very grateful for your kindness'
-    let pageIndex = '<br><br>Page index<br><br> 1. MAIN <br> 2. ABOUT <br> 3. PROJECT <br> 4. CONTACT <br><br>Please enter the number of the page you want'
+    textC += 'Welcome my PORTFOLIO<br><br>Thank you sincerely for your kind visit.<br>It meant a lot to me that you took the time to come by.<br>I know how busy you are, and i truly value the time we spent together<br><br>Please konw that I am very grateful for your kindness<br>'
+    let pageIndex = 'Page index<br><br> 1. MAIN <br> 2. ABOUT <br> 3. PROJECT <br> 4. CONTACT <br><br>Please enter the number of the page you want<br>'
     textC += pageIndex
 
-    let textBox = document.querySelector('.window > p > span');
+    let textBox = document.querySelector('.window > p:first-child > span');
     let i = 0;
     let consW = document.querySelector('.window')
-    let input = document.querySelector('.window > input');
-    let caret = document.querySelector('.window .cursor');
+    let input = document.querySelector('.window input');
+    let fakeBox = document.querySelector('.window .fake')
     // let caretX = input.getBoundingClientRect().left;
     let error = '' //입력 범위내의 숫자인지 확인해달라는 메세지
-    let errorMessage = 'Sorry'
+    let errorMessage = 'Please enter a single digit number within the range'
+    let running = false
+    let cBoxes = document.querySelectorAll('.contents > .box');
 
-    setTimeout(function () {
-        textBox.nextSibling.classList.add('on')
-        const typing = setInterval(() => {
-            if (textC.charAt(i) === '<') {
-                textBox.innerHTML += '<br>'
-                i = i + 4;
-            } else {
-                textBox.innerHTML += textC.charAt(i)
-                i++
-                i
-            }
-            if (i === textC.length) {
-                clearInterval(typing)
-                caretY = input.offsetTop;
-                caretX = input.getBoundingClientRect().left;
-                caret.style.top = `calc(${caretY}px + 1rem)`;
-                caret.style.left = caretX + 'px';
-                textBox.nextSibling.classList.add('off')
-                textBox.nextSibling.classList.remove('on')
-                caret.classList.add('on')
-                input.focus();
-            }
-        }, 30);
-    }, 3000)
+    if (running === false) {
+        running = true;
+        setTimeout(function () {
+            textBox.nextSibling.classList.add('on')
+            const typing = setInterval(() => {
+                if (textC.charAt(i) === '<') {
+                    textBox.innerHTML += '<br>'
+                    i = i + 4;
+                } else {
+                    textBox.innerHTML += textC.charAt(i)
+                    i++
+                    i
+                }
+                if (i === textC.length) {
+                    clearInterval(typing)
+                    textBox.nextSibling.classList.add('off')
+                    textBox.nextSibling.classList.remove('on')
+                    fakeBox.nextElementSibling.classList.add('on')
+                    input.focus();
+                    running = false;
+                    cBoxes[0].classList.add('load');
+                }
+            }, 1);
+        }, 3000)
+    }
 
 
     input.addEventListener('focus', function () {
         input.addEventListener('keydown', function (e) {
-            caret.classList.add('keydown')
-            let length = input.value.length
-            let caretX = input.getBoundingClientRect().left;
-            caret.style.left = `calc(${caretX}px + ${length*0.4}rem)`;
+            fakeBox.nextElementSibling.classList.add('keydown')
+            fakeBox.innerHTML = input.value
         })
         input.addEventListener('keyup', function (e) {
-            caret.classList.remove('keydown')
-            let length = input.value.length
-            let caretX = input.getBoundingClientRect().left;
-            caret.style.left = `calc(${caretX}px + ${length*0.5}rem)`;
+            fakeBox.nextElementSibling.classList.remove('keydown')
+            fakeBox.innerHTML = input.value
             let errorF = function () {
-                textBox.innerHTML = ''
-                error += errorMessage;
-                error += pageIndex
-                textBox.innerHTML += '<br><br>'
-                i = 0
-                textBox.nextSibling.classList.add('on')
-                const typing = setInterval(() => {
-                    if (error.charAt(i) === '<') {
-                        textBox.innerHTML += '<br>'
-                        i = i + 4
-                    } else {
-                        textBox.innerHTML += error.charAt(i)
-                        i++;
+                cBoxes.forEach(function(v,n){
+                    v.classList.remove('load')
+                    if (n===cBoxes.length) {
+                        v.classList.add('load')
                     }
-                    if (i === error.length) {
-                        clearInterval(typing)
-                        caretY = input.offsetTop;
-                        caretX = input.offsetLeft;
-                        caret.style.top = `calc(${caretY}px + 1rem)`;
-                        caret.style.left = caretX + 'px';
-                        textBox.nextSibling.classList.add('off')
-                        textBox.nextSibling.classList.remove('on')
-                        caret.classList.add('on')
-                        input.focus();
-                        error = '';
-                    }
-                }, 30);
+                })
+                if (running === false) {
+                    running = true;
+                    error += 'error<br><br>'
+                    error += errorMessage;
+                    error += pageIndex
+                    textBox.innerHTML += '<br><br>'
+                    i = 0
+                    textBox.nextSibling.classList.remove('off')
+                    fakeBox.nextSibling.classList.remove('on')
+                    const typing = setInterval(() => {
+                        if (error.charAt(i) === '<') {
+                            textBox.innerHTML += '<br>'
+                            i = i + 4
+                        } else {
+                            textBox.innerHTML += error.charAt(i)
+                            i++;
+                        }
+
+                        if (i === error.length) {
+                            clearInterval(typing)
+                            textBox.nextSibling.classList.add('off')
+                            fakeBox.innerHTML = ''
+                            fakeBox.nextSibling.classList.add('on')
+                            input.focus();
+                            error = '';
+                            running = false;
+                        }
+                        consW.scrollBy(0, consW.scrollHeight)
+                    }, 1);
+                }
             }
             if (e.key == 'Enter') {
+                //pageMove------------------------------------------------------
+                cBoxes.forEach(function(v,n){
+                    v.classList.remove('load')
+                })
+                let pageChk = input.value
                 textBox.nextSibling.classList.remove('off')
-                caret.classList.remove('on')
-                let inputPage = Number(input.value)
-                input.value = '';
-                if (Number.isInteger(inputPage)) {
-                    if (inputPage >= 1 && inputPage <= 4) {
-                        // contentsbox(inputPage).getbou를 0으로
-                        inputPage = ''
-                    } else {
-                        errorF();
+                fakeBox.nextElementSibling.classList.remove('on')
+                textBox.innerHTML += input.value
+                input.value = ''
+                let pageMove = ''
+
+                let success = function () {
+                    if (running === false) {
+                        running = true;
+                        pageMove += 'success<br><br>'
+                        pageMove += `Go to page no.${pageChk}<br>`
+                        pageMove += pageIndex
+                        textBox.innerHTML += '<br><br>'
+                        i = 0
+                        textBox.nextSibling.classList.remove('off')
+                        fakeBox.nextSibling.classList.remove('on')
+                        console.log(pageMove)
+                        const typing = setInterval(() => {
+                            if (pageMove.charAt(i) === '<') {
+                                textBox.innerHTML += '<br>'
+                                i = i + 4
+                            } else {
+                                textBox.innerHTML += pageMove.charAt(i)
+                                i++;
+                            }
+    
+                            if (i === pageMove.length) {
+                                clearInterval(typing)
+                                textBox.nextSibling.classList.add('off')
+                                fakeBox.innerHTML = ''
+                                fakeBox.nextSibling.classList.add('on')
+                                cBoxes.forEach(function(v,n){
+                                    if(n+1==pageChk){
+                                        v.classList.add('load')
+                                    }
+                                })
+                                input.focus();
+                                pageChk=''
+                                pageMove = '';
+                                running = false;
+                                input.value = ''
+                            }
+                            consW.scrollBy(0, consW.scrollHeight)
+                        }, 1);
                     }
+                }
+
+                if (pageChk == 1) {
+                    // contentsbox(pageChk).getbound를 0으로
+                    success();
+                } else if (pageChk == 2) {
+                    success();
+                } else if (pageChk == 3) {
+                    success();
+                } else if (pageChk == 4) {
+                    success();
                 } else {
                     errorF();
                 }
             }
-
         })
     })
 }
