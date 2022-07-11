6eb367eb021b095f6e0c875f2d0f396d90ec4981
Montegrand <hpt0311@naver.com>
2022-07-11 15:02:29 +0900

    220711

--- js/index.js
+++ js/index.js
@@ -1,16 +1,31 @@
 window.onload = function () {
 
-    let textC = '';
+    let opening = `\u00a0\u00a0\u00a0\u00a0;rrr7r7S\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0rXr7r77a\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0r20W@BZX,\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0XS77XX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0XX7r72i\u00a0\u00a0\u00a0\u00a0aXr7r7rrr7rrrrr7r7r7r772i\u00a0\u00a0\u00a0\u00a0277rrr7r7r7rrrrr7r7;<br>+
+\u00a0\u00a0\u00a0\u00a0MMMMMMMMB\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0aMMMMMMMMMMMM8;\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMZ\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMS\u00a0\u00a0\u00a0,MMMMMMMMMMMMMMMMMMMMMMMMZ\u00a0\u00a0\u00a0.MMMMMMMMMMMMMMMMMMMM<br>+
+\u00a0\u00a0\u00a0\u00a0MMMMMMMMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0rMMMMMMMMMMMMMMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMM@\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0;MMMMMMMMMMMMMMMMMMMMMMMMB\u00a0\u00a0\u00a0,MMMMMMMMMMMMMMMMMMMM<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ZMMMMMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0WMMMMMMMMMMMMMMMMMMMMM,\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMS\u00a0\u00a0\u00a0iMMMMMMMMMMMMMMMMMMMMMMMM8\u00a0\u00a0\u00a0:MMMMMMMMMMMMMMMMMMMM<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMr\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:0MMMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMMMMMMMS\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0WMMMMMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0.MMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMMMM:\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMS\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM,\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ZMMMMMMMMMMM.\u00a0\u00a0\u00a0\u00a0iMMMMMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMB\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMMMMM;\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMM7BMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMX@MMMMM.\u00a0\u00a0\u00a0\u00a0WMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0aMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM0MMMMMMS\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMM:\u00a0MMMMMS\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMM0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0MMMMMMZ\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0MMMMMM;\u00a0MMMMMMZ\u00a0\u00a0\u00a0\u00a0MMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0.MMMMMMB0WWWWWBWBW@M\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMMX\u00a0aMMMMM\u00a0\u00a0\u00a0\u00a0\u00a00MMMMM\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0MMMMMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMr\u00a0\u00a0\u00a0\u00a0MMMMMMS\u00a0\u00a0MMMMMMB\u00a0\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM:\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMMMMMMMMMMMMMMM\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMMS\u00a0\u00a0MMMMMM\u00a0\u00a0\u00a0iMMMMMS\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0MMMMMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMr\u00a0\u00a0\u00a0\u00a0MMMMMM2\u00a0\u00a0\u00a0BMMMMMM\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMMMMMMMMMMMMMMM\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMMS\u00a0\u00a0:MMMMMS\u00a0\u00a0MMMMMM\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0MMMMMM8\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM:\u00a0\u00a0\u00a0\u00a0MMMMMMZ\u00a0\u00a0\u00a0\u00a08MMMMMM\u00a08MMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM:\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMMX7XSXXXXXSX22\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a08MMMMM\u00a02MMMMM\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a08MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a08MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0SMMMMMMMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMMS\u00a0\u00a0\u00a0\u00a0MMMMMMMMMMMS\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0.MMMMMM0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0iMMMMMMZ\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0rMMMMMMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM:\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a0\u00a0:MMMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0aMMMMMMB\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0rMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0WMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a08MMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a00MMMMMMMZ;\u00a0\u00a0\u00a0\u00a0,SMMMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM.\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0SMMMMMMMMMMMMMMMMMMMMW\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMa\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0,MMMMMMMMMMMMMMMMMMMM<br>+
+\u00a0\u00a0\u00a0\u00a0@MMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0iMMMMMM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMM,\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a00MMMMMMMMMMMMMMMMM;\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMZ\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0WMMMMMM2\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMMMMMi\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0:MMMMMMMMMMMMMMMMMMMM<br>+
+\u00a0\u00a0\u00a0\u00a0BW@WMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0SWBB@\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MMW@MM\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0.S8W@MMMMM@Bai\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0WMWW@MX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0XM@WMMX\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0MM@WMM,\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0M@@WWW@W@WWWWW@WWW@B<br><br>`
+    let openingB = opening.split("+");
 
-    textC += 'Welcome to my PORTFOLIO<br><br>Thank you sincerely for your kind visit.<br>It meant a lot to me that you took the time to come by.<br>I know how busy you are, and i truly value the time we spent together<br><br>Please know that I am very grateful for your kindness<br><br>';
+    let openingT = `안녕하세요. 저의 포트폴리오에 방문해 주셔서 감사합니다.<br><br>저는 앞날을 건실하고 견고하게 나아가고 싶은,<br>웹 개발에 첫발을 내딘<br>홍기현입니다.<br><br>이 포트폴리오가<br>저의 웹 개발을 바라보는 시선이<br>방문하신분께 잘 전될되길 바랍니다.<br><br>`
+
+    let textC = '';
     let pageIndex = 'Page index, <br><br>1. MAIN, <br> 2. ABOUT, <br> 3. PROJECT, <br> 4. CONTACT, <br><br>Please enter the number of the page you want<br>';
     let indexArr = pageIndex.split(",");
-    indexArr.forEach(function (v, n) {
-        textC += v;
-        if (n === 1) {
-            textC += ' (now)'
-        }
-    })
 
     let textBox = document.querySelector('.window > p:first-child > span');
     let i = 0;
@@ -25,7 +40,11 @@ window.onload = function () {
     let contents = document.querySelector('.contents');
     let line = document.querySelector('.line');
     let myPhoto = document.querySelector('.contents > .fst img')
-    console.log(myPhoto)
+    let cons = document.querySelector('.console')
+
+    consW.addEventListener('click',function(){
+        input.focus();
+    })
 
     cBoxes.forEach(function (v, n) {
         v.addEventListener('mousemove', function (e) {
@@ -44,10 +63,24 @@ window.onload = function () {
         }
     })
 
+    console.log(textC)
+
     if (running === false) {
         running = true;
+        textC += openingT;
+        textC += 'Welcome to my PORTFOLIO<br><br>Thank you sincerely for your kind visit.<br>It meant a lot to me that you took the time to come by.<br>I know how busy you are, and i truly value the time we spent together<br><br>Please know that I am very grateful for your kindness<br><br>';
+        indexArr.forEach(function(v,n){
+            textC += v;
+        });
+        const open = setInterval(()=> {
+            textBox.innerHTML += openingB[i]
+            i++
+            if (i===openingB.length){
+                clearInterval(open)
+                i = 0;
+            }
+        },100)
         setTimeout(function () {
-            contents.classList.add('loading');
             textBox.nextSibling.classList.add('on');
             const typing = setInterval(() => {
                 if (textC.charAt(i) === '<') {
@@ -60,22 +93,29 @@ window.onload = function () {
                 consW.scrollBy(0, consW.scrollHeight)
                 if (i === textC.length) {
                     clearInterval(typing)
+                    line.style.top = `${window.innerHeight*0.67}px`
+                    contents.style.height = `${(window.innerHeight*0.67)-3}px`
+                    cons.style.height = `${(window.innerHeight*0.33)+3}px`
                     textBox.nextSibling.classList.add('off')
                     fakeBox.nextElementSibling.classList.add('on')
-                    input.focus();
                     running = false;
-                    cBoxes[0].classList.add('load');
-                    contents.classList.remove('loading')
                 }
             }, 30);
         }, 3000)
     }
 
+    cons.addEventListener('transitionend',function(){
+        input.focus();
+        consW.scrollBy(0, consW.scrollHeight)
+    })
 
     input.addEventListener('focus', function () {
         input.addEventListener('keydown', function (e) {
-            fakeBox.nextElementSibling.classList.add('keydown')
-            fakeBox.innerHTML = input.value
+            fakeBox.nextElementSibling.classList.add('keydown');
+            fakeBox.innerHTML = input.value;
+            cons.classList.remove('open');
+            contents.classList.remove('open');
+            line.classList.remove('open');
         })
         input.addEventListener('keyup', function (e) {
             fakeBox.nextElementSibling.classList.remove('keydown')
@@ -203,6 +243,14 @@ window.onload = function () {
         window.addEventListener('mouseup', function () {
             window.removeEventListener('mousemove', resize)
         })
+    })
+
+    window.addEventListener('resize',function(){
+        let cons = document.querySelector('.console')
+        console.log(window.innerHeight - (contents.offsetHeight+6))
+        line.style.top = `${contents.offsetHeight-3}px`
+        // contents.style.height = `${line.offsetTop}px`
+        cons.style.height = `${window.innerHeight - (contents.offsetHeight+6)}px`
     })
 
 }
